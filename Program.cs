6e4384e4942085d32e100829fd1d3598f80ed104
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 08:28:42 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Gra_w_życie__Conwaya;
+
+// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    internal static void Main()
+    {
+        uint poczatkowa_liczba_zywywch_komurek = Convert.ToUInt32(Console.ReadLine());
+
+        List <Kordy> lista_korduw = new List<Kordy>();
+        
+        for (int i = 0; i < poczatkowa_liczba_zywywch_komurek; i++) 
+        {
+            Kordy kordy = new Kordy();
+            kordy.x = Convert.ToUInt32(Console.ReadLine());
+            kordy.y = Convert.ToUInt32(Console.ReadLine());
+
+            lista_korduw.Add(kordy);
+        }
+
+        Symulator_gry_w_życie test = new(lista_korduw);
+
+        Console.Write("Podaj agar: ");
+        uint agar = Convert.ToUInt32(Console.ReadLine());
+
+        test.CyklRzycia(agar);
+    }
+}
+
+
+
---
+++ Symulator_gry_w_życie.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_w_życie__Conwaya;
+
+class Symulator_gry_w_życie
+{
+    internal const uint dlugosc_boku_planszy = 6;
+
+    internal bool[,] plansza = new bool[dlugosc_boku_planszy, dlugosc_boku_planszy];
+
+    internal Symulator_gry_w_życie(List<Kordy> lista_korduw)
+    {
+        Inicjalizuj();
+
+        foreach (Kordy iterator in lista_korduw)
+        {
+            this.plansza[iterator.x, iterator.y] = true;
+        }
+    }
+
+    private void Inicjalizuj()
+    {
+        for (uint i = 0; i < dlugosc_boku_planszy; i++)
+        {
+            for (uint j = 0; j < dlugosc_boku_planszy; j++)
+            {
+                this.plansza[i, j] = false;
+            }
+        }
+    }
+
+    internal Symulator_gry_w_życie()
+    {
+        Inicjalizuj();
+    }
+
+    uint PoliczRzywychSomsiaduw(uint x, uint y)    //w tzw. sąsiedztwie Moore'a
+    {
+        uint licznik = 0;
+
+        if (x > 0)          //w informatyce zanczynamy od 0
+        {
+            if (y > 0)
+            {
+                if (this.plansza[x - 1, y - 1])
+                    licznik++;
+            }
+            if (y < dlugosc_boku_planszy - 1)
+            {
+                if (plansza[x-1, y+1])
+                    licznik++;
+            }
+            if (this.plansza[x - 1, y])
+                licznik++;
+        }
+        if (x < dlugosc_boku_planszy-1)
+        {
+            if (this.plansza[x + 1, y])
+                licznik++;
+            if (y > 0)
+            {
+                if (this.plansza[x + 1, y - 1])
+                    licznik++;
+            }
+            if (y < dlugosc_boku_planszy-1)
+            {
+                if (this.plansza[x + 1, y + 1])
+                    licznik++;
+            }
+        }
+        if (y > 0)
+        {
+            if (this.plansza[x, y - 1]) licznik++;
+        }
+        if (y < dlugosc_boku_planszy-1)
+        {
+            if (this.plansza[x, y + 1]) licznik++;
+        }
+
+        return licznik;
+    }
+
+    protected void Iteruj()
+    {
+        Symulator_gry_w_życie nowa_faza = new Symulator_gry_w_życie();
+
+        for (uint i = 0; i<dlugosc_boku_planszy; i++)
+        {
+            for (uint j = 0; j<dlugosc_boku_planszy; j++)
+            {
+                uint liczba_rzywych_somsiaduw = PoliczRzywychSomsiaduw(i, j);
+                nowa_faza.plansza[i, j] = (liczba_rzywych_somsiaduw == 2  ||  liczba_rzywych_somsiaduw == 3);
+            }
+        }
+
+        this.plansza = nowa_faza.plansza;
+    }
+
+    internal void CyklRzycia(uint agar)
+    {
+        for (uint i = 0; i < agar; i++)
+        {
+            Iteruj();
+            Console.WriteLine(this.ToString());
+            Console.WriteLine();
+        }
+    }
+
+    public override string ToString()
+    {
+        string tekst = "";
+
+        for (uint i = 0; i < dlugosc_boku_planszy; i++)
+        {
+            for (uint j = 0; j < dlugosc_boku_planszy; j++)
+            {
+                tekst += plansza[i, j].ToString();
+                tekst += " ";
+            }
+            tekst += "\n";
+        }
+
+        return tekst;
+    }
+}
+
+struct Kordy
+{
+    public uint x;
+    public uint y;
+
+    public Kordy(uint nowy_x, uint nowy_y)
+    {
+        this.x = nowy_x;
+        this.y = nowy_y;
+    }
+
+    public Kordy()
+    { }
+
+    public static bool operator==(Kordy a, Kordy b)
+    {
+        return a.x == b.x && a.y == b.y;
+    }
+
+    public static bool operator!=(Kordy a, Kordy b)
+    {
+        return a.x != b.x || a.y != b.y;
+    }
+
+    public override string ToString()
+    {
+        return String.Join(" ", this);
+    }
+}
